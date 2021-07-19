befe82b37aaa47f208f74bac51e80b9798c0daab
IzyanzZ <87628633+IzyanzZ@users.noreply.github.com>
2021-07-19 12:07:38 +0700

    Create Main.php

---
+++ src/Izyan/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Izyanz;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+    public function onLoad(): void {
+        $this->getLogger()->info("[Izyan] Plugin Loaded");
+    }
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("[Izyan] Plugin Enable");
+    }
+
+    public function onDisable(): void {
+        $this->getLogger()->info("[Izyan] Plugin Disable);
+    }
+}
